0d3a032353fdea1d1035f4d3975401b8aa0ea377
Nadia27 <jones.nadia.l@gmail.com>
2018-03-17 11:38:38 -0400

    initial commit

---
+++ app/models/resources.js
@@ -0,0 +1,51 @@
+// Dependencies
+// =============================================================
+
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "Chirp" model that matches up with DB
+var asd_resources = sequelize.define("ASD_resources", {
+    Name: {
+        type: Sequelize.STRING,
+        notNull: true
+    },
+    Location: {
+        type: Sequelize.STRING,
+        notNull: true
+    },
+    Phone_Number: {
+        type: Sequelize.INTEGER,
+        not: ["[a-z]", 'i'],     // will not allow letters
+        notNull: true
+    },
+    Description: {
+        type: Sequelize.STRING,
+        notNull: true
+    },
+    Price: {
+        type: Sequelize.STRING,
+        notNull: true
+    },
+    Insurance_Accecpted: {
+        type: Sequelize.BOOLEAN,
+        notNull: true
+    },
+    Catergory: {
+        type: Sequelize.INTEGER,
+        notNull: true
+    },
+    Rating: {
+        type: Sequelize.INTEGER,
+        notNull: true
+    },
+}, {
+        timestamps: false
+    });
+
+// Syncs with DB
+ASD_resources.sync();
+
+// Makes the ASD Model available for other files (will also create a table)
+module.exports = ASD_resources;
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the "ASD_resources" if it exists currently --
+DROP DATABASE IF EXISTS ASD_resources;
+-- Creates the "ASD_resources" database --
+CREATE DATABASE ASD_resources;
