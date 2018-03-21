2f9f1ee81e6ef51ad30dfc1297b3b92e1e85f522
Jalil <>
2018-03-21 13:38:02 -0400

    updated firebase login

--- public/assets/js/authentication.js
+++ public/assets/js/authentication.js
@@ -9,75 +9,35 @@
     messagingSenderId: "293019946548"
   };
   firebase.initializeApp(config);
+// Get elements
+var inputEmail = document.getElementById('inputEmail');
+var inputPassword = document.getElementById('inputPassword');
+var btnSignup = document.getElementById('createAccount');
+var btnLogin = document.getElementById('login');
 
-
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      document.getElementById('user_div').style.display = 'initial';
-      document.getElementById('login_div').style.display = 'none';
-      
-
-      var user = firebase.auth().currentUser;
+// Add login event
+btnLogin.addEventListener('click', e => {
+  var email = inputEmail.value;
+  var pass = inputPassword.value;
+  var auth = firebase.auth();
+  // Sign in
+  var promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
+});
+// signup event
+btnSignup.addEventListener('click', e =>{
+    var email = inputEmail.value;
+  var pass = inputPassword.value;
+  var auth = firebase.auth();
+  // Sign in
+  var promise = auth.createUser WithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
+});
+firebase.auth().omAuthStateChanged(firebaseUser => {
+  if(firebaseUser) {
+    console.log(firebaseUser);
+    }else {
+      console.log('not logged in');
     }
-
-    if(user != null){
-
-        var email_id = user.email;
-        var email_verified = user.emailVerified;
-
-        if(email_verified){
-          document.getElementById('sendVerfication').style.display = 'none';
-       
-        } else{
-          document.getElementById('sendVerfication').style.display = 'initial';
-          
-        }
-
-        document.getElementById('user_para').innerHTML = 'User : ' + email_id + '<br><br>Currently you email verification status is : ' + email_verified;
-    } 
-
-    else {
-      // No user is signed in.
-       document.getElementById('user_div').style.display = 'none';
-      document.getElementById('login_div').style.display = 'initial';
-     
-
-    }
-});
-
-
-
-
-$('#login').on('click', function(){
-    var userEmail = document.getElementById('inputEmail').value;
-    var userPassword = document.getElementById('inputPassword').value;
-
-   firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-
-      alert('Error : ' + errorMessage);
-
-    });
-});
-
-
-$('#createAccount').on('click', function(){
-
-    var userEmail = document.getElementById('inputEmail').value;
-    var userPassword = document.getElementById('inputPassword').value;
-
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-
-      alert('Error : ' + errorMessage);
-
-    });
-
-});
+  }
+})
